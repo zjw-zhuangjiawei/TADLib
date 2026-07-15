467193d2de0bdd62d68d75b8ec4ef00418f3839c
Zhuang Jiawei <zjw@stu.scau.edu.cn>
2026-07-15 12:35:46 +0800

    replace pkg_resources.parse_version with packaging.version.parse

--- tadlib/__init__.py
+++ tadlib/__init__.py
@@ -4,21 +4,24 @@
 # Organization: HuaZhong Agricultural University
 
 import xmlrpc.client
-from pkg_resources import parse_version as V
+from packaging.version import parse as V
 
-__author__ = 'XiaoTao Wang'
-__version__ = '0.4.4'
-__license__ = 'GPLv3+'
+__author__ = "XiaoTao Wang"
+__version__ = "0.4.4"
+__license__ = "GPLv3+"
 
 ## Check for update
 try:
-    pypi = xmlrpc.client.ServerProxy('http://pypi.python.org/pypi')
-    available = pypi.package_releases('TADLib')
+    pypi = xmlrpc.client.ServerProxy("http://pypi.python.org/pypi")
+    available = pypi.package_releases("TADLib")
     if V(__version__) < V(available[0]):
-        print('*'*75)
-        print('Version {0} is out of date, Version {1} is available.'.format(__version__, available[0]))
+        print("*" * 75)
+        print(
+            "Version {0} is out of date, Version {1} is available.".format(
+                __version__, available[0]
+            )
+        )
         print()
-        print('*'*75)
+        print("*" * 75)
 except:
     pass
-
